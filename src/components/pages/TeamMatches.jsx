7dcec6af19bceb2df723f3f04a338deee0560a0c
MatCenteleghe <167896011+MatCenteleghe@users.noreply.github.com>
2024-08-19 13:49:33 -0300

    update loading matches

--- src/components/pages/TeamMatches.jsx
+++ src/components/pages/TeamMatches.jsx
@@ -62,63 +62,68 @@ function TeamMatches() {
   return (
     <section className={styles.matches_section}>
       <NavLeague btn1Clicked="btn" btn2Clicked="btn_clicked" />
-      <div className={styles.btn_container}>
-        <AddButton text="Criar Partida" addMatch={() => setModalShow(true)} />
-      </div>
       {loading ? (
         <p className={styles.loading}>Carregando...</p>
       ) : (
-        <div className={styles.matches_container}>
-          {getMatches.length === 0 ? (
-            <p></p>
-          ) : (
-            <p>Selecione a partida para editá-la</p>
-          )}
-          <AddMatch
-            teams={teams}
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-          />
-
-          <EditMatch
-            match={match}
-            show={modalEditShow}
-            onHide={() => setModalEditShow(false)}
-          />
-
-          <div className={styles.matches_area}>
-            {getMatches.length === 0 ? (
-              <p>Não há pártidas definidas</p>
-            ) : (
-              getMatches.map((match, index) => (
-                <div className={styles.match_area} key={index}>
-                  <div
-                    key={index}
-                    className={styles.match}
-                    onClick={() => getMatchId(match)}
-                  >
-                    <div>
-                      <p>P{index + 1} |</p>
-                    </div>
-                    <p>
-                      <span>{match.team1}</span>
-                      <span>
-                        {match.goalTeam1 || ""} X {match.goalTeam2 || ""}
-                      </span>
-                      <span>{match.team2}</span>
-                    </p>
-                  </div>
-                  <button
-                    onClick={() => removeMatch(match.id)}
-                    className={styles.deleteBtn}
-                  >
-                    <BsFillTrashFill />
-                  </button>
-                </div>
-              ))
-            )}
+        <>
+          <div className={styles.btn_container}>
+            <AddButton
+              text="Criar Partida"
+              addMatch={() => setModalShow(true)}
+            />
           </div>
-        </div>
+          <div className={styles.matches_container}>
+            {getMatches.length === 0 ? (
+              <p></p>
+            ) : (
+              <p>Selecione a partida para editá-la</p>
+            )}
+            <AddMatch
+              teams={teams}
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
+
+            <EditMatch
+              match={match}
+              show={modalEditShow}
+              onHide={() => setModalEditShow(false)}
+            />
+
+            <div className={styles.matches_area}>
+              {getMatches.length === 0 ? (
+                <p>Não há pártidas definidas</p>
+              ) : (
+                getMatches.map((match, index) => (
+                  <div className={styles.match_area} key={index}>
+                    <div
+                      key={index}
+                      className={styles.match}
+                      onClick={() => getMatchId(match)}
+                    >
+                      <div>
+                        <p>P{index + 1} |</p>
+                      </div>
+                      <p>
+                        <span>{match.team1}</span>
+                        <span>
+                          {match.goalTeam1 || ""} X {match.goalTeam2 || ""}
+                        </span>
+                        <span>{match.team2}</span>
+                      </p>
+                    </div>
+                    <button
+                      onClick={() => removeMatch(match.id)}
+                      className={styles.deleteBtn}
+                    >
+                      <BsFillTrashFill />
+                    </button>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
+        </>
       )}
       <Footer />
     </section>
